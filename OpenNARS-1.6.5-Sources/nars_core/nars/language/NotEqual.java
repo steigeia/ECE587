a6d3c4332840921c6add96013a73f2a53090692c
Isaac Steiger <steigeia@miamioh.edu>
2018-12-04 12:48:16 -0500

    Removed un-needed code and printout statements in NotEquals

--- OpenNARS-1.6.5-Sources/nars_core/nars/language/NotEqual.java
+++ OpenNARS-1.6.5-Sources/nars_core/nars/language/NotEqual.java
@@ -19,23 +19,18 @@ import nars.operator.NotEqualOperation;
  * A Statement about an Inheritance relation.
  */
 public class NotEqual extends Statement {
-
-	static public Vector<Vector<String>> s = new Vector<Vector<String>>();
 	
-//	public static int NotEqualCount = -1;
 	public static Vector<Term> v1 = new Vector<Term>();
 	public static Vector<Term> v2 = new Vector<Term>();
 	public static boolean contains(final Term subject, final Term predicate) {
 		for(int i = 0; i < NotEqual.v1.size(); i++) {
 	        if(NotEqual.v1.get(i).equals(subject) && NotEqual.v2.get(i).equals(predicate) ||
 	        		NotEqual.v2.get(i).equals(subject) && NotEqual.v1.get(i).equals(predicate)) {
-	        	System.out.println("Prevented Inheritence");
 	    		return true;
 	        }
     	}
 		return false;
 	}
-	//static public List listNotEqual = (List) new ArrayList();  
 	
     /**
      * Constructor with partial values, called by make
@@ -82,11 +77,8 @@ public class NotEqual extends Statement {
      * @return A compound generated or null
      */
     public static NotEqual make(final Term subject, final Term predicate) {
-//    	NotEqualCount= NotEqualCount + 1;
     	v1.add(subject);
     	v2.add(predicate);
-//    	System.out.println(v1.get(NotEqualCount));
-//    	System.out.println(v2.get(NotEqualCount));
     	
         if (subject==null || predicate==null || invalidStatement(subject, predicate)) {            
             return null;
